36de11f72eb0ba35d7075795ebdbd076339f8502
Pedro Henrique <phcasttro@outlook.com>
2025-10-21 15:43:16 -0300

    adicionado métodos de envio de email

---
+++ src/main/java/com/phc/notificacao/business/EmailService.java
@@ -0,0 +1,57 @@
+package com.phc.notificacao.business;
+
+import com.phc.notificacao.business.dto.TarefasDTO;
+import com.phc.notificacao.infra.exceptions.EmailException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    public EmailService(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    @Value("${envio.email.remetente}")
+    public String remetente;
+
+    @Value("${envio.email.nomeRemetente}")
+    private String nomeRemetente;
+
+    public void enviarEmail(TarefasDTO tarefasDTO) {
+        try {
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mensagem, true, StandardCharsets.UTF_8.name());
+
+            mimeMessageHelper.setFrom(new InternetAddress(remetente, nomeRemetente));
+            mimeMessageHelper.setTo(InternetAddress.parse(tarefasDTO.getEmailUsuario()));
+            mimeMessageHelper.setSubject("Notificação de Tarefas");
+
+            Context context = new Context();
+            context.setVariable("nomeTarefa", tarefasDTO.getNomeTarefa());
+            context.setVariable("dataEvento", tarefasDTO.getDataEvento());
+            context.setVariable("descricao", tarefasDTO.getDescricao());
+            String template = templateEngine.process("notificacao", context);
+            mimeMessageHelper.setText(template, true);
+            javaMailSender.send(mensagem);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new EmailException("Error ao enviar o email " , e.getCause());
+        }
+    }
+
+}
